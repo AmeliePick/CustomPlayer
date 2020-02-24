fe12836fc9a04028c98dbbb9a21b50b4ba7e56a8
AmeliePick <ameliepickdev@gmail.com>
2020-02-24 20:37:29 +0300

    feature(project) Add project's files.

---
+++ CustomPlayer_Vi/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomPlayer_Vi")]
+[assembly: AssemblyDescription("The mod for Grand Theft Auto V. Changes the model of any of the protagonists of the game to model V from Cyberpunk 2077, changes the character's voice and some his animations.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomPlayer_Vi")]
+[assembly: AssemblyCopyright("AmeliePick ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("82641b5a-5875-41e0-921a-0dd850dad7bb")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
