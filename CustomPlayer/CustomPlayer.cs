ceacb10e4ece1dab9f23b0ed5f62860d4a865c2a
AmeliePick <ameliepickdev@gmail.com>
2020-04-24 19:08:05 +0300

    feature(CustomPlayer.cs) Add menu for clothing changing.

--- CustomPlayer/CustomPlayer.cs
+++ CustomPlayer/CustomPlayer.cs
@@ -49,7 +49,7 @@ namespace CustomPlayer
         UIMenu customizeMenu;
         UIMenuItem about;
 
-
+        enum BodyPart { HEAD = 0, HAIR = 2, FACE = 1, EYES = 7, TORSO = 3, TORSO2 = 11, HANDS = 5, LEGS = 4, FEET = 6 };
 
         public CustomPlayer()
         {
@@ -79,14 +79,14 @@ namespace CustomPlayer
             // Init events
             mainMenu.OnItemSelect += onMainMenuItemSelect;
             KeyDown += OnKeyDown;
-            Tick += OnTick;  
+            Tick += OnTick;
         }
 
         void OnTick(object sender, EventArgs e)
         {
             if (modMenuPool != null)
                 modMenuPool.ProcessMenus();
-           
+
         }
 
         void OnKeyDown(object sender, KeyEventArgs e)
@@ -111,7 +111,7 @@ namespace CustomPlayer
             {
                 string inputText = GTA.Game.GetUserInput(WindowTitle.FMMC_KEY_TIP9N, 60);
 
-                if(inputText != "")
+                if (inputText != "")
                 {
                     gameClass.SaveCharacter(inputText);
 
@@ -121,11 +121,11 @@ namespace CustomPlayer
                     UI.ShowSubtitle("Name can not be empty!");
 
             }
-            else if(item == about)
+            else if (item == about)
             {
                 BigMessageThread.MessageInstance.ShowSimpleShard("Custom Player",
-                "Developed by AmeliePick — https://ameliepick.ml"); 
-            } 
+                "Developed by AmeliePick — https://ameliepick.ml");
+            }
         }
 
 
@@ -144,7 +144,7 @@ namespace CustomPlayer
             UILoadCharacter = new UIMenuItem("Load the character", "Load your saved character.");
             UILoadMenu.AddItem(UILoadCharacter);
 
-            UILoadDefaultPlayer = new UIMenuItem("Load the default", 
+            UILoadDefaultPlayer = new UIMenuItem("Load the default",
                                                "Load the default character, " +
                                                "If you are currently using the added character model, " +
                                                "then use this button to load the character model, " +
@@ -168,7 +168,7 @@ namespace CustomPlayer
                 {
                     LoadMenuInit();
                 }
-                else if(MenuItems.Count > 1)
+                else if (MenuItems.Count > 1)
                 {
                     MenuItems.RemoveAt(0);
                     MenuItems.Insert(0, new UIMenuListItem("Select your hero:", ListOfNames, 0, "Load the selected character."));
@@ -188,7 +188,7 @@ namespace CustomPlayer
         void SubMenuCharactersListSetup()
         {
             this.UILoadMenu = modMenuPool.AddSubMenu(mainMenu, "Load", "Change your current player character to a saved character.");
-            
+
 
             ListOfNames = new List<dynamic>();
             previousCountOfNamesList = 0;
@@ -198,7 +198,7 @@ namespace CustomPlayer
 
             if (ListOfNames.Count == 0)
             {
-                
+
                 UILoadMenuEmptyButton = new UIMenuItem("", "You have no saved characters yet.");
                 UILoadMenu.AddItem(UILoadMenuEmptyButton);
             }
@@ -216,7 +216,7 @@ namespace CustomPlayer
                     else
                         UI.ShowSubtitle("Character was loaded");
                 }
-                else if(item == UILoadDefaultPlayer)
+                else if (item == UILoadDefaultPlayer)
                 {
                     gameClass.LoadDefaultPlayer();
 
@@ -224,7 +224,7 @@ namespace CustomPlayer
                 }
             };
 
-            
+
         }
 
 
@@ -232,7 +232,7 @@ namespace CustomPlayer
         {
             customizeMenu = modMenuPool.AddSubMenu(mainMenu, "Customize");
 
-            
+
             // VOICE MENU //
             UIMenu UIvoiceMenu = modMenuPool.AddSubMenu(customizeMenu, "Voice", "Change character's voice.");
             UIvoiceMenu.SetMenuWidthOffset(100);
@@ -240,7 +240,7 @@ namespace CustomPlayer
 
 
             List<dynamic> voiceList = new List<dynamic>(gameClass.LoadVoiceList());
-            
+
             UIMenuListItem voiceChangeList = new UIMenuListItem("", voiceList, 0, "Change character's voice.");
             UIvoiceMenu.AddItem(voiceChangeList);
 
@@ -254,11 +254,11 @@ namespace CustomPlayer
 
             UIvoiceMenu.OnItemSelect += (sender, item, index) =>
             {
-                if(item == voiceTest)
+                if (item == voiceTest)
                 {
                     gameClass.PlaySpeechByPlayer(voiceList[voiceChangeList.Index]);
                 }
-                else if(item == applyNewVoice)
+                else if (item == applyNewVoice)
                 {
                     gameClass.SetNewVoice(voiceList[voiceChangeList.Index]);
                 }
@@ -267,14 +267,77 @@ namespace CustomPlayer
 
 
             // CLOTHING MENU //
-            UIMenu clothingMenu = modMenuPool.AddSubMenu(customizeMenu, "Clothing", "Change the clothes of the model, if she has one");
+            UIMenu UIclothingMenu = modMenuPool.AddSubMenu(customizeMenu, "Clothing", "Change the clothes of the model, if she has one");
 
-            //List<dynamic> headList = new List<dynamic>();
-            //UIMenuListItem head = new UIMenuListItem("Change head:", headList, 0, "Change character's head.");
-            //clothingMenu.AddItem(head);
+            // Add submenus with elements
+            BodyPart bodyPart = BodyPart.HEAD;
+            for(int i = 0; i < 1; ++i)
+            {
+                UIMenuItem _UIMenuItem = new UIMenuItem(bodyPart.ToString());
+                UIclothingMenu.AddItem(_UIMenuItem);
+
+                UIMenu submenu = new UIMenu(_UIMenuItem.Text, _UIMenuItem.Text);
 
 
-            clothingMenu.OnItemSelect += (sender, item, index) =>
+                
+                List<dynamic> clothingDrawID = new List<dynamic>(gameClass.getNumberOfDrawable((int)bodyPart));
+                for (int num = 0; num < clothingDrawID.Capacity; num++)
+                {
+                    clothingDrawID.Add(num);
+                }
+                UIMenuListItem drawableID = new UIMenuListItem("Drawable", clothingDrawID, clothingDrawID.IndexOf(gameClass.getCurrentDrawableID((int)bodyPart)));
+
+                if (clothingDrawID.Count < 2) drawableID.Enabled = false;
+                else
+                {
+                    drawableID.OnListChanged += (sender, newIndex) =>
+                    {
+                        string componentTitleID = "";
+                        foreach (var element in UIclothingMenu.MenuItems)
+                        {
+                            
+                        }
+
+                        gameClass.setDrawableID(componentId: (int)Enum.Parse(typeof(BodyPart), componentTitleID),
+                                            value: sender.IndexToItem(sender.Index));
+                    };
+                }
+
+                
+                submenu.AddItem(drawableID);
+
+
+
+                List<dynamic> clothingTextureID = new List<dynamic>(gameClass.getNumberOfTexture((int)bodyPart));
+                for (int num = 0; num < clothingTextureID.Capacity; num++)
+                {
+                    clothingTextureID.Add(num);
+                }
+                UIMenuListItem textureID = new UIMenuListItem("Texture", clothingTextureID, clothingTextureID.IndexOf(gameClass.getCurrentTextureID((int)bodyPart)));
+
+                if (clothingTextureID.Count < 2) textureID.Enabled = false;
+                else
+                {
+                    textureID.OnListChanged += (sender, newIndex) =>
+                    {
+                        gameClass.setTextureID(componentId: (int)Enum.Parse(typeof(BodyPart), sender.IndexToItem(sender.Index)),
+                                               value: sender.IndexToItem(sender.Index));
+                    };
+                }
+
+
+                submenu.AddItem(textureID);
+
+                modMenuPool.Add(submenu);
+                UIclothingMenu.BindMenuToItem(submenu, _UIMenuItem);
+
+                ++bodyPart;
+            }
+
+            
+
+
+            UIclothingMenu.OnItemSelect += (sender, item, index) =>
             {
                 
             };
